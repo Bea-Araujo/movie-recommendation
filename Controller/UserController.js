779557fc145d28909d7cc231a4b70d6391755c74
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-10-27 16:31:37 -0300

    usermodel e base de usercontroller criados

---
+++ Controller/UserController.js
@@ -0,0 +1,22 @@
+
+function user(app) {
+    app.get('/allUsers', (req, res) => {
+
+    })
+
+    app.get('/user/:id', (req, res) => {
+
+    })
+
+    app.post('/newUser', (req, res) => {
+
+    })
+
+    app.put('/editUser/:id', (req, res) => {
+
+    })
+
+    app.delete('/deleteUser/:id', (req, res) => {
+
+    })
+}
---
+++ Model/UserModel.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(userName, password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    //future: validação de senha, encriptar senha
+    //talvez adicionar campo de email
+
+}
