d3d1254a79cebf5fba986f5647d587a7833771a1
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-10-28 14:45:08 -0300

    adição de cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { follow } from './src/Controller/FollowsController.js';
 import { post } from './src/Controller/PostController.js';
 import { user } from './src/Controller/UserController.js';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
